034dde97c64d4cc51370b5812da176aac6c0603a
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-01 22:07:30 -0500

    put this on version control

---
+++ day1.fs
@@ -0,0 +1,9 @@
+module Day1
+
+open System
+open System.Numerics
+open System.Text.RegularExpressions
+
+let main =
+    let input = System.IO.File.ReadAllText(@"/home/alan/hdd/code/aadvent/input/1.txt")
+    Console.Write(input)

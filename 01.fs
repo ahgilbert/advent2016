66c6fc4067c106063368422af969c0b249ce06f3
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-04 16:09:00 -0500

    on the scoreboard!

--- 01.fs
+++ 01.fs
@@ -22,10 +22,30 @@ let readOrder (h :: ts) =
 
 let pivot dir (lat,long) =
     match dir with
-    | R -> if lat = 0 then (-long, 0) else (0, lat)
-    | L -> if lat = 0 then (long, 0) else (0, -lat) 
+    | R -> if lat = 0
+           then (-long, 0)
+           else (0, lat)
+    | L -> if lat = 0
+           then (long, 0)
+           else (0, -lat) 
+
+let scalarMultiply (x,y) z =
+    (x*z, y*z)
+
+let addTuples (a,b) (c,d) =
+    (a+c, b+d)
+
+let step dir dist head loc =
+    let newDir = pivot dir head
+    let scaled = scalarMultiply newDir dist
+    (newDir, addTuples loc scaled)
+
+let followOneOrder (heading, location) (dir, dist) =
+    step dir dist heading location
 
 let main =
-    let input = System.IO.File.ReadLines(@"/home/alan/hdd/code/aadvent/input/01.txt")
+    let input = Seq.cache <| System.IO.File.ReadLines(@"/home/alan/hdd/code/aadvent/input/01.txt")
     let orders = Seq.map readOrder (Seq.map Seq.toList input)
-    Console.WriteLine(Seq.length input)
+    let (_, (lat,long)) = Seq.fold followOneOrder ((1,0),(0,0)) orders
+    Console.WriteLine("Lat: " + string lat + " Long: " + string long)
+    Console.WriteLine("Santa will travel " + string (Operators.abs lat + Operators.abs long) + " blocks")

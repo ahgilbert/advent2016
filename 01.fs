ef3af76f8f70267e3851769f0a5e2f0c4126f3a9
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-04 15:44:39 -0500

    pivot compiles and appears to work

--- 01.fs
+++ 01.fs
@@ -13,14 +13,19 @@ let castDir d =
     match d with
     | 'R' -> R
     | 'L' -> L
-    | x -> raise (Bunk("bad turn direction"))
+    | x -> raise <| Bunk("bad turn direction")
 
 let readOrder (h :: ts) =
     let dir = castDir h
     let faith = ts |> System.String.Concat |> Int32.Parse
     (dir, faith)
 
+let pivot dir (lat,long) =
+    match dir with
+    | R -> if lat = 0 then (-long, 0) else (0, lat)
+    | L -> if lat = 0 then (long, 0) else (0, -lat) 
+
 let main =
-    let input = System.IO.File.ReadLines(@"/home/alan/hdd/code/aadvent/input/1.txt")
+    let input = System.IO.File.ReadLines(@"/home/alan/hdd/code/aadvent/input/01.txt")
     let orders = Seq.map readOrder (Seq.map Seq.toList input)
     Console.WriteLine(Seq.length input)

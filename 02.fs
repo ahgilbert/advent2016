57cd2935844c7abdd736c19030d7db1b291933f4
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-07 12:28:09 -0500

    making progress, but struggling with module imports

--- 02.fs
+++ 02.fs
@@ -16,32 +16,50 @@ let readDirection c =
     | 'D' -> Down
     |  _  -> raise <| Bunk "bad direction"
 
-let boundedMove (x,y) d =
-    let squeeze n = max 1 <| min keypad_size n
-    let squeeze' (a,b) = (squeeze a, squeeze b)
-    match d with
-    | Right -> squeeze' (x + 1, y)
-    | Left -> squeeze' (x - 1, y)
-    | Up -> squeeze' (x, y-1)
-    | Down -> squeeze' (x, y+1)
+let numPad1 =
+    [ for a in 1..3 do
+      for b in 1..3 do
+          yield (a,b) ]
+    |> (fun xs -> Seq.zip xs "123456789")
+    |> Set.ofSeq
+
+let numPad2 =
+    Seq.concat [[(3,1)];
+                [ for x in 2..4 do yield (x,2) ];
+                [ for x in 1..5 do yield (x,3) ];
+                [ for x in 2..4 do yield (x,4) ];
+                [(3,5)]]
+    |> (fun xs -> Set.zip xs "123456789ABCD")
+    |> Set.ofSeq
+
+let boundedMove validKeys (x,y) d =
+    let newLoc =
+        match d with
+        | Right -> (x + 1, y)
+        | Left -> (x - 1, y)
+        | Up -> (x, y-1)
+        | Down -> (x, y+1)
+    if Set.contains newLoc validKeys
+    then newLoc
+    else (x,y)
 
 let getDigit (x,y) =
     x + ((y - 1) * keypad_size)
     
-let faith start orders =
-    Seq.fold boundedMove start orders
+let getNextKey keypad start orders =
+    Seq.fold (boundedMove keypad) start orders
 
 let readInput file =
     Seq.cache <| System.IO.File.ReadLines(file)
     |> Seq.map Seq.toList
 
-let day2 file =
+let day2a file =
     readInput file
     |> Seq.map (Seq.map readDirection)
-    |> Seq.scan faith (2,2)
+    |> Seq.scan (getNextKey (map fst numPad1)) (2,2)
     |> Seq.tail
     |> Seq.map getDigit
     |> Seq.map string
     |> Seq.map Console.WriteLine
     
-let main = day2 @"/home/alan/hdd/code/aadvent/input/02.txt"
+let main = day2a @"/home/alan/hdd/code/aadvent/input/02.txt"

0d3eeef5e63b7252dd16f09c4ec1bdf2c3087326
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-08 16:13:14 -0500

    further factoring

--- 03.fs
+++ 03.fs
@@ -8,12 +8,12 @@ let readInput = System.IO.File.ReadLines "input/03.txt"
 let seqToTriplet threes =
     (Seq.item 0 threes, Seq.item 1 threes, Seq.item 2 threes)
 
-let parseInputA =
+let parseInputA input =
     let faith (s : string) = s.Split ' '
                              |> Seq.filter (fun x -> x <> "") 
                              |> Seq.map Int32.Parse
                              |> seqToTriplet
-    Seq.map faith
+    Seq.map faith input
 
 let legit ((a,b,c) : (int * int * int)) =
     let sorted = Seq.sort [a;b;c] |> Seq.rev
@@ -22,7 +22,8 @@ let legit ((a,b,c) : (int * int * int)) =
     Seq.sum rest > big
 
 let main =
-    let input = Seq.cache readInput |> parseInputA
-    let part1 = input |> Seq.filter legit
-                      |> Seq.length
+    let input = Seq.cache readInput
+    let inputA = parseInputA input
+    let part1 = inputA |> Seq.filter legit
+                       |> Seq.length
     Console.WriteLine ("part 1 shows " + string part1 + " illegal triangles")

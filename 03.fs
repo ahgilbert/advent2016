c1df53902a6a1e7e117fc805a1eef0e7adc67620
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-08 16:11:17 -0500

    factor out some stuff, still get correct answer

--- 03.fs
+++ 03.fs
@@ -3,12 +3,17 @@ module D3
 
 open System
 
-let readInput =
-    let faith (s : string) = s.Split ' ' |> Seq.filter (fun x -> x <> "") 
+let readInput = System.IO.File.ReadLines "input/03.txt"
+
+let seqToTriplet threes =
+    (Seq.item 0 threes, Seq.item 1 threes, Seq.item 2 threes)
+
+let parseInputA =
+    let faith (s : string) = s.Split ' '
+                             |> Seq.filter (fun x -> x <> "") 
                              |> Seq.map Int32.Parse
-                             |> fun is -> (Seq.item 0 is, Seq.item 1 is, Seq.item 2 is)
-    System.IO.File.ReadLines "input/03.txt"
-    |> Seq.map faith
+                             |> seqToTriplet
+    Seq.map faith
 
 let legit ((a,b,c) : (int * int * int)) =
     let sorted = Seq.sort [a;b;c] |> Seq.rev
@@ -17,7 +22,7 @@ let legit ((a,b,c) : (int * int * int)) =
     Seq.sum rest > big
 
 let main =
-    let input = Seq.cache readInput
+    let input = Seq.cache readInput |> parseInputA
     let part1 = input |> Seq.filter legit
                       |> Seq.length
     Console.WriteLine ("part 1 shows " + string part1 + " illegal triangles")

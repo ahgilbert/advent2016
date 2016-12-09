21d326c2abf4192adf0c0658b673df25c89a5892
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-09 12:30:20 -0500

    copied from Stephen

--- 03.fs
+++ 03.fs
@@ -2,47 +2,41 @@
 module D3
 
 open System
+open System.IO
 
-let sideways xs =
-    let xs = Seq.map (fun a -> Seq.item 0 a) xs
-    let ys = Seq.map (fun a -> Seq.item 1 a) xs
-    let zs = Seq.map (fun a -> Seq.item 2 a) xs
-    Seq.concat [ xs; ys; zs; ]
+let readLines path =
+    File.ReadAllLines path
 
-let seqToTriplet threes =
-    (Seq.item 0 threes, Seq.item 1 threes, Seq.item 2 threes)
+let split (delimiter:string) (input:string) =
+    input.Split([|delimiter|], StringSplitOptions.RemoveEmptyEntries)
 
-let readInput =
-    let faith (s : string) = s.Split ' '
-                             |> Seq.filter (fun x -> x <> "") 
-                             |> Seq.map Int32.Parse
-    Seq.cache <| System.IO.File.ReadLines "input/03.txt"
-    |> Seq.map faith
+let input =
+    let lines = readLines "input/03.txt"
+    lines
+    |> Seq.map (split " " >> (Seq.map int) >> Seq.toList)
+    |> Seq.toList
 
-let parseInputA input =
-    Seq.map seqToTriplet input
+let legit [x;y;z] =
+    x + y > z && y + z > x && x + z > y
 
-let rec chunk n xs =
-    match Seq.isEmpty xs with
-    | true -> seq []
-    | false ->
-      let zz = Seq.take n xs
-      let yy = Seq.skip n xs
-      Seq.append (seq [zz]) (chunk n yy) 
+let part1 =
+    input
+    |> Seq.filter legit
+    |> Seq.length
 
-let parseInputB input =
-    let a = sideways input
-    "ahg"
-
-let legit ((a,b,c) : (int * int * int)) =
-    let sorted = Seq.sort [a;b;c] |> Seq.rev
-    let big = Seq.head sorted
-    let rest = Seq.tail sorted
-    Seq.sum rest > big
+let part2 =
+    let input' =
+        let rec loop xs = seq {
+            match xs with
+            | [] -> ()
+            | [x;y;z]::[x';y';z']::[x'';y'';z'';]::xs' ->
+                yield! [[x;x';x''];[y;y';y''];[z;z';z'']]
+                yield! loop xs' }
+        loop input
+    input'
+    |> Seq.filter legit
+    |> Seq.length
 
 let main =
-    let input = Seq.cache readInput
-    let inputA = parseInputA input
-    let part1 = inputA |> Seq.filter legit
-                       |> Seq.length
-    Console.WriteLine ("part 1 shows " + string part1 + " illegal triangles")
+    Console.WriteLine part1
+    Console.WriteLine part2

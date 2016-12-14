e288b6d2bdde411295547bdc1d5b06d98fac5511
Alan Gilbert <alanhgilbert@gmail.com>
2016-12-13 22:07:57 -0500

    don't quite get mutable values

--- 05.fs
+++ 05.fs
@@ -15,15 +15,33 @@ let hash (input : string) =
     |> Array.map (fun (x : byte) -> System.String.Format("{0:X2}", x))
     |> String.concat ""
 
-let allHashes = Seq.map hash allSeeds
+let allHashes =
+    Seq.map hash allSeeds
+    |> Seq.cache
 
-let goodHashes = Seq.filter (fun (x : string) -> x.StartsWith "00000") allHashes
+let goodHashes =
+    Seq.filter (fun (x : string) -> x.StartsWith "00000") allHashes
 
-let p1 =
+let p1 () =
     goodHashes
     |> Seq.take 8
     |> Seq.map (Seq.skip 5 >> Seq.head >> string)
     |> String.concat ""
 
-let main =
-    Console.WriteLine p1
+let p2 =
+    let mutable (slots : Map<int, char>) = Map.empty
+    let checkHash (h : string) =
+        let i = Seq.item 6 h |> string |> Int32.Parse
+        let c = Seq.item 7 h
+        if i > 7 then false
+        elif Map.containsKey i slots then false
+        else
+            printf "%i : %c" i c
+            slots <- Map.add i c slots
+            true
+    goodHashes
+    |> Seq.filter checkHash
+    |> Seq.take 8
+    |> ignore
+    slots
+

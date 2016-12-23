ead79b6fe49daea7d2d09b6f71b9a3bdeaf56f7c
Alan Gilbert <alan.gilbert@relayfoods.com>
2016-12-22 20:40:54 -0500

    add move and generateMoves functions

--- 11.fsx
+++ 11.fsx
@@ -1,6 +1,5 @@
 type Machine = Chip of string | Gen of string
 type Scene = int * list<list<Machine>>
-type Move = U | D
 
 let testInput =
     (0, [[Chip "H"; Chip "Li"]; [Gen "H"]; [Gen "Li"]; []])
@@ -13,6 +12,13 @@ let input =
       []])
 
 let all = List.fold (&&) true
+
+let rec getPairs xs =
+    if (List.length xs < 2) then []
+    else
+        let h = List.head xs
+        let headPairs = List.map (fun x -> [h; x]) (List.tail xs)
+        List.append headPairs (getPairs (List.tail xs))
 
 let checkVictory (_, scene) =
     List.take 3 scene
@@ -38,4 +44,34 @@ let explodes equipment =
 let isLegalState (level, distribution) =
     level >= 0 
     && level <= 4
-    && List.map (explodes >> not) distribution |> all
+    && List.map (explodes >> not) distribution
+    |> all
+
+let move dir cargo (currentFloor, floors) =
+// update game state
+    let destFloor = currentFloor + dir
+    let staysPut = fun m -> not <| List.contains m cargo
+    if destFloor > 4 || destFloor < 0
+    then (-1, floors) // force illegal game state, prune this move
+    else
+        let thisFloor' = List.filter staysPut (List.item currentFloor floors)
+        let newFloor' = List.append cargo (List.item destFloor floors)
+        let floorDoer = fun i ->
+            if i = currentFloor then thisFloor'
+            elif i = destFloor then newFloor'
+            else List.item i floors
+        let newFloors = List.map floorDoer [0..3] // Replace new floor and old floor...
+        (destFloor, newFloors)
+
+let generateMoves (currentFloor, floors) =
+    let directions =
+        if currentFloor = 0 then [1]
+        elif currentFloor = 3 then [-1]
+        else [1;-1]
+    let stuffHere = List.item currentFloor floors
+    let singleCargo = List.map (fun x -> [x]) stuffHere
+    let possibleCargo = List.append singleCargo (getPairs stuffHere)
+    [for dir in directions do
+     for cargo in possibleCargo do
+     yield (dir, cargo, (currentFloor, floors))]
+    |> Seq.toList
